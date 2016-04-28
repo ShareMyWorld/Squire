b813c4f284746b9ce30cf7f4cdb95add7da808ea
jonas hadin <kronprinserik@gmail.com>
2016-04-28 11:39:51 +0200

    static blocks in the beginning gets a treatment

--- build/squire-raw.js
+++ build/squire-raw.js
@@ -692,13 +692,14 @@ function fixContainer ( container, root ) {
             fixParagraph( wrapper, container, squire, doc );
             wrapper = null;
         }
-        if ( isContainer( child ) && child.nodeName !== 'LI' && child.isContentEditable ) {
-            if ( fixStaticBlocks( child, squire, doc, config ) ) {
+        if ( fixStaticBlocks( child, squire, doc, config ) ) {
                 i += 1;
                 l += 1;
-            }
+        }
+        if ( isContainer( child ) && child.nodeName !== 'LI' && child.isContentEditable ) {
             fixContainer( child, root );
         }
+        
     }
     if ( container === root || ( isContainer( container ) && !/^[OU]L$/.test( container.nodeName ) ) ) {
         squire._ensureBottomLine( container );
@@ -716,9 +717,9 @@ function fixStaticBlocks( node, squire, doc, config ) {
     var classification = squire._allowedContent[ smwNode ];
     var isStatic = classification === 'blockAtomic' || classification === 'containers'; 
     
-    var previous;
+    var previous = node.previousSibling;
     var nodeInsertedBefore = false;
-    if ( isStatic && (previous = node.previousSibling) ) {
+    if ( isStatic ) {
         var smwPrevious = squire._translateToSmw[ getFullNodeName( previous ) ];
         var prevClassification = squire._allowedContent[ smwPrevious ];
         switch ( prevClassification ) {
@@ -4830,6 +4831,7 @@ proto.insertPageBreak = function ( ) {
 
     if ( range.collapsed ) {
         var endP = getNearest( range.endContainer, self._root, 'P' );
+        endP.parentNode.insertBefore( self.createDefaultBlock( [ ] ), block.nextSibling );
         endP.parentNode.insertBefore( block, endP.nextSibling );
         endP.parentNode.insertBefore( self.createDefaultBlock( [ ] ), block.nextSibling );
     } else {
@@ -5007,7 +5009,7 @@ proto.toggleEm = function () {
 };
 
 proto.toggleHr = function () {
-    //Only adds 
+    
     this.insertPageBreak();
 };
 
